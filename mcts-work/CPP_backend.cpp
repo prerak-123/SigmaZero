ca9596a49b58cc1e110859f9d7f2b7e3cb420522
hrishi-06 <hrishijedhe06@gmail.com>
2023-10-23 03:47:47 +0530

    integrating python in cpp

--- mcts-work/CPP_backend.cpp
+++ mcts-work/CPP_backend.cpp
@@ -1,4 +1,5 @@
 #include <boost/python.hpp>
+#include <Python.h>
 #include <boost/multiprecision/cpp_dec_float.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 #include <cstdlib>
@@ -8,64 +9,91 @@
 
 #define ll long long int
 
+class C_Node;
+class C_Edge;
+class C_MCTS;
+class C_Edge;
+
 class C_Node{
     public:
         std::string state;
         int turn;
         std::vector<C_Edge*> edges;
         boost::multiprecision::cpp_int N;
-        boost::multiprecision::cpp_dec_float value;
+        boost::multiprecision::cpp_dec_float_50 value;
 
         C_Node(std::string state);
-        std::string step(C_Action* action);
+        std::string step(boost::python::object action);
         bool is_game_over();
         bool is_leaf();
-}
+        C_Edge* add_child(C_Node* child, boost::python::object action, boost::multiprecision::cpp_dec_float_50 prior);
+        std::vector<C_Node*> get_all_children();
+        void get_all_children_helper(std::vector<C_Node*> &children);
+        C_Edge* get_edge(boost::python::object action);
+};
 
 class C_Edge{   
     public:
         C_Node* input_node;
         C_Node* output_node;
-        C_Action* action;
+        boost::python::object action;
 
         bool player_turn;
 
         boost::multiprecision::cpp_int N;
-        boost::multiprecision::cpp_dec_float W;
-        boost::multiprecision::cpp_dec_float P;
+        boost::multiprecision::cpp_dec_float_50 W;
+        boost::multiprecision::cpp_dec_float_50 P;
+
+        C_Edge(C_Node* input_node, C_Node* output_node, boost::python::object action, boost::multiprecision::cpp_dec_float_50 prior);
 };
 
-class C_Action{
+// class C_Action{
 
-}
+// }
 
 class C_MCTS{
     public:
         C_Node* root;
-        std::vector<Node*> game_path;
-        std::string cur_board;
+        std::vector<C_Node*> game_path;
+        boost::python::object cur_board;
         // TODO: put agent in here somehow
         bool stochastic;
 
-        run_simulations(int n);
+        void run_simulations(int n);
         C_Node* select_child(C_Node* node);
-        void map_valid_move(/*TODO somthing here*/);    
-}
+        void map_valid_move(boost::python::object move);
+        std::unordered_map<boost::python::object, boost::multiprecision::cpp_dec_float_50> probabilities_to_actions(vector<vector<vector<boost::multiprecision::cpp_dec_float_50>>> probabilities, std::string board); 
+        C_Node* expand(C_Node* leaf);
+        C_Node* backpropagate(C_Node* end_node, boost::multiprecision::cpp_dec_float_50 value);
+
+        C_MCTS::C_MCTS(boost::python::object agent, std::string state, bool stochastic = false);
+};      
 
 C_Node::C_Node(std::string state){
     this->state = state;
-    this->turn = ; // TODO: Insert python function here
+    // this->turn = ; // TODO: Insert python function here
     this->N = 0;
     this->value = 0;
 }
 
-std::string C_Node::step(C_Action* action){
-    std::string new_state = ; // TODO: Insert python function here
+std::string C_Node::step(boost::python::object action){
+    Py_Initialize();
+    boost::python::object chess_module = boost::python::import("chess");
+    // boost::python::object Move_from_chess = chess_module.attr("Move");
+    boost::python::object board = chess_module.attr("Board")(this->state);
+    // boost::python::object board = Board_function(this->state);
+    boost::python::object new_board = board.attr("push")(action);
+    std::string new_state = boost::python::extract<std::string>(new_board.attr("fen")());
+    Py_Finalize();
     return new_state;    
 }
 
 bool C_Node::is_game_over(){
-    bool is_game_over = ; // TODO: Insert python function here
+    Py_Initialize();
+    boost::python::object chess_module = boost::python::import("chess");
+    boost::python::object board = chess_module.attr("Board")(this->state);
+    bool is_game_over = boost::python::extract<bool>(board.attr("is_game_over")());
+    Py_Finalize();
     return is_game_over;
 }
 
@@ -73,21 +101,19 @@ bool C_Node::is_leaf(){
     return this->N == 0;
 }
 
-Edge* C_Node::add_child(C_Node* child, Action* action, boost::multiprecision::cpp_dec_float prior){
-    Edge* edge = new Edge(this, child, prior);
+C_Edge* C_Node::add_child(C_Node* child, boost::python::object action, boost::multiprecision::cpp_dec_float_50 prior){
+    C_Edge* edge = new C_Edge(this, child, action, prior);
     this->edges.push_back(edge);
     return edge;
 }
 
-std::vector<Node*> C_Node::get_all_children(){
-    std::vector<Node*> children;
+std::vector<C_Node*> C_Node::get_all_children(){
+    std::vector<C_Node*> children;
     get_all_children_helper(children);
-    return children;
-
     return children;
 }
 
-void C_Node::get_all_children_helper(std::vector<Node*> &children){
+void C_Node::get_all_children_helper(std::vector<C_Node*> &children){
     for (int i = 0; i < this->edges.size(); i++){
         children.push_back(this->edges[i]->output_node);
         assert(this->edges[i]->output_node != NULL);
@@ -95,9 +121,9 @@ void C_Node::get_all_children_helper(std::vector<Node*> &children){
     }
 }
 
-Edge* C_Node::get_edge(Action* action){
+C_Edge* C_Node::get_edge(boost::python::object action){
     for (int i = 0; i < this->edges.size(); i++){
-        if (/*Implement equality of C_Action and put it here*/){
+        if (action == this->edges[i]->action){
             return this->edges[i];
         }
     }
@@ -141,5 +167,37 @@ C_Node* select_child(C_Node* node){
 
         return best_edge->output_node;
     }
+
+    return NULL;
 }
 
+C_Edge::C_Edge(C_Node* input_node, C_Node* output_node, boost::python::object action, boost::multiprecision::cpp_dec_float_50 prior){
+    this->input_node = input_node;
+    this->output_node = output_node;
+    this->action = action;
+
+    std::string temp = input_node->state;
+    int end = temp.find(" ");
+    temp.erase(temp.begin(), temp.begin() + end + 1);
+    end = temp.find(" ");
+    this->player_turn = temp.substr(0, end) == "w";
+
+    this->N = 0;
+    this->W = 0;
+    this->P = prior;
+}
+
+boost::multiprecision::cpp_dec_float_50 C_Edge::upper_confidence_bound(boost::multiprecision::cpp_dec_float_50 noise){
+    // TODO : Implement this shit
+    return 0;
+}
+
+C_MCTS::C_MCTS(boost::python::object agent, std::string state, bool stochastic = false){
+    this->root = new C_Node(state);
+    this->agent = agent;
+    this->stochastic = stochastic;
+}
+
+// int main(){
+//     std::cout << "hello" << std::endl;
+// }
