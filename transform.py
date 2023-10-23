648d45a3bee345def6559b3f3ee0a9c44acbd79b
AdityaYadav793 <yaditya703@gmail.com>
2023-10-23 16:40:23 +0530

    Added move translation

--- transform.py
+++ transform.py
@@ -1,8 +1,10 @@
 import chess
 import torch
 
+
 def board_to_input(board: chess.Board):
-    """Coverts the given board position into the planes 
+    """
+    Coverts the given board position into the planes 
     required as input for the neural network.
     """
     pieces = []
@@ -10,7 +12,7 @@ def board_to_input(board: chess.Board):
         for piece_type in chess.PIECE_TYPES:
             plane = torch.zeros(1, 8, 8)
             for index in list(board.pieces(piece_type, color)):
-                plane[0][7 - int(index/8)][index % 8] = True
+                plane[0][7 - index//8][index % 8] = True
                 
             pieces.append(plane)
             
@@ -26,3 +28,38 @@ def board_to_input(board: chess.Board):
     claim_draw = torch.ones(1, 8, 8) if board.can_claim_draw() else torch.zeros(1, 8, 8)
     
     return torch.cat((*pieces, turn, moves, wk_castle, wq_castle, bk_castle, bq_castle, claim_draw), dim=0)
+
+
+def move_to_output(move: chess.Move):
+    knight_moves = [-15, -17, -6, 10, 15, 17, 6, -10]
+    
+    diff = move.to_square - move.from_square
+    index = 0
+    
+    if move.promotion == chess.QUEEN or move.promotion is None:
+        if diff not in knight_moves:
+            if diff%8 == 0:
+                index = abs(diff)//8
+                
+            elif move.to_square//8 == move.from_square//8:
+                index = 14 + abs(diff)
+                
+            elif move.to_square%8 < move.from_square%8:
+                index = 28
+                dist = diff - (move.to_square%8 - move.from_square%8)
+                index += abs(dist)//8
+                    
+            else:
+                index = 42
+                dist = diff - (move.to_square%8 - move.from_square%8)
+                index += abs(dist)//8
+                
+            if diff < 0:
+                    index += 7
+                    
+        else:
+            index = 56 + knight_moves.index(diff) + 1
+    else:
+        index = 64 + 3*(move.promotion - 2) + (abs(diff) - 6)
+        
+    return index-1
